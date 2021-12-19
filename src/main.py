686e00328871851918ab60a34b82d22cab9a5c08
Domepo <dominiktb2@gmail.com>
2021-12-19 21:32:50 +0100

    Third

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
 import json
 from icalendar import Calendar, Event, Alarm
 from datetime import date, datetime, timedelta
-
 from icalendar.caselessdict import CaselessDict
+
+class iCalendar:
+	def __init__(self, file):
+		self.file = file
+		self.cal = Calendar()
+
+		self.cal.add('version', '2.0')
+		self.cal.add('prodid', 'Dome und Leon Parser')
+
+	def add(self, dateStart, dateEnd, summary, location='', description='', categories=''):
+		event = Event()
+
+		event.add('summary', summary)
+		event.add('location', location)
+		event.add('categories', categories, encode=0)
+		event.add('description', description)
+		event.add('dtstart', dateStart)
+		event.add('dtend', dateEnd)
+		event.add('dtstamp', datetime.now())
+		event.add('priority', 5)
+
+		self.cal.add_component(event)
+
+	def save(self):
+		f = open(self.file, 'wb')
+		f.write(self.cal.to_ical())
+		f.close()
 
 
 class icalGen:
@@ -91,14 +117,43 @@ class icalGen:
                     weeksDividedByNamesFormat[p].append(l)
 
         return weeksDividedByNamesFormat
-   
+
+    def hoursDelta(self, hoursV):
+        return timedelta(hours = hoursV)
+
     def writeIcal(self):
         formatIcal = self.formatIcal()
 
-        print(formatIcal["Andreas"])
+        #print(formatIcal)
+        a = iCalendar("/Users/dominik/Desktop/Github/icalGenerator/src/a.ics")
+        for i in formatIcal:
+            #print(i)
+            for c in formatIcal[i]:
+                #print(c)
+                daytime = datetime.strptime(c,"%Y-%m-%d" + "-T" + "%H%M%S")
+                #
+                # Todo:
+                # get value from {"MI":10, "Do":10,"So":10}
+                #
+                if(daytime.weekday() == 2):
+                    startDate =  daytime +  self.hoursDelta(18)  
+                elif(daytime.weekday() == 4):
+                    startDate =  daytime +  self.hoursDelta(19)  
+                elif(daytime.weekday() == 6):
+                    startDate =  daytime +  self.hoursDelta(10)  
+                else:
+                    startDate =  daytime +  self.hoursDelta(10)  
+
+                endDate = startDate + self.hoursDelta(1)
+                #
+                #
+                #
+
+                a.add(startDate,endDate,i+" "+self.team,"EFG",i,"Tontechnik")
+        a.save()
 
 
 
-
-Bild = icalGen("Bild", ["MI", "Do"], ["Dominik", "Peter","Andreas"])
+Bild = icalGen("Bild", {"MI":10, "Do":10,"So":10}, ["Dominik", "Peter","Andreas"])
 Bild.writeIcal()
+
